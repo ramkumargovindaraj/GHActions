2d5277847750f77bfee63fe3d3e4e9f67fc08353
ramkumar.cs@outlook.com <Satya@231285>
2026-03-26 16:03:23 +0100

    updated

--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -3,4 +3,4 @@ import requests
 def test_status_code():
     url = "https://api.github.com"
     response = requests.get(url)
-    assert response.status_code == 200
+    assert response.status_code == 201
